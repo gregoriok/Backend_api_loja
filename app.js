7712df7c20a8ff6c6ce62e19c3ac735a92dd62d8
Gregório Kessler Steinke <42988007+gregoriok@users.noreply.github.com>
2024-01-23 15:04:08 -0300

    cors correction

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connnectDB from "./src/config/dbConnect.js";
 import routes from "./src/routes/index.js";
+import cors from 'cors'
 
 const conexao = await connnectDB();
 
@@ -13,6 +14,12 @@ conexao.on("error",(erro =>{
   })
 // var session = require('express-session')
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 routes(app);
 
 export default app;
